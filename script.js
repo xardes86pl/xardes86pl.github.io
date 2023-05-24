9c8cb03a7ac2347565aa1e3ab19e5effbe1f9045
xardes86pl <103959490+xardes86pl@users.noreply.github.com>
2023-05-24 17:34:09 +0200

    Remade everything better

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const sr = ScrollReveal({
     distance: "50px",
     duration: 3000,
     delay: 500,
-    reset: false,
+    reset: false
   });
 
 
-sr.reveal(".sec1", { delay: 150, origin: "top" });
-sr.reveal("#section2", { delay: 150, origin: "top" });
-sr.reveal("#header", { delay: 150, origin: "top" });
+sr.reveal("header", { delay: 150, origin: "top" });
+sr.reveal(".parent1", { delay: 150, origin: "top" });
+sr.reveal(".parent2", { delay: 150, origin: "top" });
