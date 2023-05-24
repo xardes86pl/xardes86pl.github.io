034aff82c383147c1b2d3cd61ff49930d4537e05
xardes86pl <103959490+xardes86pl@users.noreply.github.com>
2023-05-24 12:08:14 +0200

    Updated script

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("header");
-
-// Get the offset position of the navbar
-var header = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > header) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-
-
-
-
-
-
-
-
-
 const sr = ScrollReveal({
     distance: "50px",
     duration: 3000,
